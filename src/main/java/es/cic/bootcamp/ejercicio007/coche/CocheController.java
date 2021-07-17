c4e5a7ce0e29d898d2078edaa1e30d5699013e2e
rpovedano <rpovedano@gmail.com>
2021-07-17 12:22:53 +0200

    Creación CocheService. Inyección de dicha dependencia en el controller. Comprobación de la integración del test. CocheService.encender() devuelve UnsupportedOperationException

--- src/main/java/es/cic/bootcamp/ejercicio007/coche/CocheController.java
+++ src/main/java/es/cic/bootcamp/ejercicio007/coche/CocheController.java
@@ -1,13 +1,19 @@
 package es.cic.bootcamp.ejercicio007.coche;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CocheController {
 
+	@Autowired 
+	private CocheService cocheService;
+	
+	
+	
 	@GetMapping("encender")
 	public int encender() {
-		throw new UnsupportedOperationException("Operación no implementada todavía");
+		return cocheService.encender();
 	}
 }
---
+++ src/main/java/es/cic/bootcamp/ejercicio007/coche/CocheService.java
@@ -0,0 +1,11 @@
+package es.cic.bootcamp.ejercicio007.coche;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CocheService {
+
+	public int encender () {
+		throw new UnsupportedOperationException("Operación no implementada todavía");
+	}
+}
