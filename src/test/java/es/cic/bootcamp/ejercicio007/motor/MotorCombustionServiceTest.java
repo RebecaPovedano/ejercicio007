0a9924f2c65dc4fba2cdf49f88ca433dbf47d03b
rpovedano <rpovedano@gmail.com>
2021-07-17 12:56:42 +0200

    Test unitarios de los servicios relativos a los motores.

---
+++ src/test/java/es/cic/bootcamp/ejercicio007/motor/MotorCombustionServiceTest.java
@@ -0,0 +1,31 @@
+package es.cic.bootcamp.ejercicio007.motor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MotorCombustionServiceTest {
+
+private MotorCombustionService cut;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		cut=new MotorCombustionService();
+	}
+
+	@Test
+	void testArrancarPotenciaMinima() {
+		int potencia=cut.arrancar();
+		assertTrue(potencia>=5, 
+				String.format("La potencia es menor de 5. La potencia es %d",potencia));
+	}
+
+	@Test
+	void testArrancarPotenciaMaxima() {
+		int potencia=cut.arrancar();
+		assertTrue(potencia<=40, 
+				String.format("La potencia es mayor de 40. La potencia es %d",potencia));
+	}
+
+}
